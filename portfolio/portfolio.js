8780cd32bb8e85e626d2abd781718dc15b8c7000
pole-afk <mihajlopostic223@gmail.com>
2025-05-19 18:23:22 +0200

    portfolio.sajt

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,4 +1,23 @@
-function toggleDetails(card) {
-    const details = card.querySelector('.details');
-    details.style.display = details.style.display === 'block' ? 'none' : 'block';
+const themeToggle = document.getElementById('theme-toggle');
+const htmlElement = document.documentElement; // Dobijamo <html> element
+
+// Funkcija za promenu teme
+function toggleTheme() {
+    if (htmlElement.classList.contains('dark')) {
+        htmlElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    } else {
+        htmlElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    }
 }
+
+// Provera sačuvane teme pri učitavanju stranice
+if (localStorage.getItem('color-theme') === 'dark' || (!localStorage.getItem('color-theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    htmlElement.classList.add('dark');
+} else {
+    htmlElement.classList.remove('dark');
+}
+
+// Dodajemo event listener na dugme
+themeToggle.addEventListener('click', toggleTheme);
